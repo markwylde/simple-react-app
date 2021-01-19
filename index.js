174597beb77bc992b2e8a0aeea245a61b95b4842
Mark Wylde <me@markwylde.com>
2021-01-19 13:05:46 +1000

    Implement using hooks

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
+const React = require('react');
+const { useState } = require('react');
+const ReactDOM = require('react-dom');
 
-const element = React.createElement(
-  'div',
-  {className: 'greeting'},
-  'Hello, world!'
-);
+const h = React.createElement;
+
+function Clicker ({ count, setCount }) {
+  return h('button', {
+    onClick: () => {
+      setCount(count + 1);
+    }
+  }, count);
+}
+
+function App ({ value }) {
+  const [count, setCount] = useState(0);
+
+  return h('div', { className: 'greeting' }, 'Hello ' + value,
+    h(Clicker, { count, setCount })
+  );
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-  ReactDOM.render(element, document.getElementById('myApp'))
-})
+  const ui = h(App, { value: 'Laura' });
+  ReactDOM.render(ui, document.getElementById('myApp'));
+});
